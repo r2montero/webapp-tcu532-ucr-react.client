0cddc93019ff28e0a646eb3c5d7e5c996940c702
Edgar <edgar.rojas@ucrso.info>
2021-04-12 17:35:37 -0600

    header, must need color change

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Inicio",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Talleres",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Temas",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Infografias",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Salir",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
